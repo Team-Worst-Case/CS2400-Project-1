40ea2f349d8ac58d7ec7721c48a4cf34c0b201ac
Christopher <programmerchristopher@gmail.com>
2021-09-25 22:41:57 -0700

    minor bug fix for ArrayBagTest

--- ArrayBagTest.java
+++ ArrayBagTest.java
@@ -1,27 +1,26 @@
  import java.util.Arrays;
-    
-    public class ArrayBagTest{
-        
-        public static void main(String[]args){
-            BagInterface<String>b1 = new ResizeableArrayBag<String>(); // create two bags
-            BagInterface<String>b2 = new ResizeableArrayBag<String>();
+  
+public class ArrayBagTest
+{
+    public static void main(String[]args){
+        BagInterface<String>b1 = new ResizeableArrayBag<String>(); // create two bags
+        BagInterface<String>b2 = new ResizeableArrayBag<String>();
 
-          b1.add("a"); // add strings
-          b1.add("b");
-          b1.add("c");
-          b2.add("b");
-          b2.add("b");
-          b2.add("d");
-          b1.add("e");
+      b1.add("a"); // add strings
+      b1.add("b");
+      b1.add("c");
+      b2.add("b");
+      b2.add("b");
+      b2.add("d");
+      b1.add("e");
 
-          System.out.println("bag1: " + Arrays.toString(b1.toArray())); // print bag1 and bag2
-          System.out.println("bag2: " + Arrays.toString(b2.toArray()));
+      System.out.println("bag1: " + Arrays.toString(b1.toArray())); // print bag1 and bag2
+      System.out.println("bag2: " + Arrays.toString(b2.toArray()));
 
-          System.out.println("bag1 union bag2: " + Array.toString(b1.union(b2).toArray())); // test union, intersection, and difference method.
+      System.out.println("bag1 union bag2: " + Arrays.toString(b1.union(b2).toArray())); // test union, intersection, and difference method.
 
-          System.out.println("bag1 intersection bag2: " + Array.toString(b1.intersection(b2).toArray()));
+      System.out.println("bag1 intersection bag2: " + Arrays.toString(b1.intersection(b2).toArray()));
 
-          System.out.println("bag1 difference bag2: " + Array.toString(b1.difference(b2).toArray()));
-        }
+      System.out.println("bag1 difference bag2: " + Arrays.toString(b1.difference(b2).toArray()));
     }
 }
